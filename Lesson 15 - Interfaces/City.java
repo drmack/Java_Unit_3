10c6fc3df51c58c8faad4eff8cccc92e379f59d7
SageMoon4702 <33138040+SageMoon4702@users.noreply.github.com>
2019-09-30 13:47:32 -0400

    9-30-19 U3L15 uploaded
    lesson 15 uploaded but not fixed

---
+++ Lesson 15 - Interfaces/City.java
@@ -0,0 +1,16 @@
+public class City
+{
+    private int population;
+    private String name;
+
+    public City(String name, int population)
+    {
+        this.name = name;
+        this.population = population;
+    }
+    
+    public String getName()
+    {
+        return this.name;
+    }
+}
---
+++ Lesson 15 - Interfaces/Fraction.java
@@ -0,0 +1,4 @@
+public class Fraction implements Comparable<Fraction>
+{
+    
+}
